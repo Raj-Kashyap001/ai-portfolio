615fc068baecf05117708b48ffc3a428313d6578
Raj Kashyap <rajk917958@gmail.com>
2025-12-23 07:34:43 +0000

    Try fixing this error: `Console Error: Error fetching from https://gener

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,9 +2,10 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
 import {config} from 'dotenv';
 
-config({ path: `.env` });
+config({path: `.env.local`});
+config({path: `.env`});
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [googleAI({apiKey: process.env.GEMINI_API_KEY})],
   model: 'googleai/gemini-2.5-flash',
 });
